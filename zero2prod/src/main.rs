cb2bbdde235a4e831e7c7235fff2a513d49e0999
Marius <marius.ihring@icloud.com>
2023-09-06 18:19:12 +0200

    refactoring project / book page 64

---
+++ zero2prod/src/main.rs
@@ -0,0 +1,8 @@
+use std::net::TcpListener;
+use zero2prod::startup::run;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("failed to bind to random port");
+
+    run(listener)?.await
+}
